97bc845456d6d5200fe161ef056efdaac5867e84
vasily.smolin <vasilysmoolin@gmail.com>
2022-09-06 14:08:49 +0400

    lint stan test 6

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,15 +2,19 @@
 
 namespace App\Http\Controllers;
 
+
 use App\Http\Resources\UserResource;
 use App\Http\Resources\UsersResource;
 use App\Models\User;
+use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    public function index(): UsersResource
+    public function index(Request $request): UsersResource
     {
-        return new UsersResource(User::all());
+        $take = $request->get('take') ?? 100;
+        $skip = $request->get('skip') ?? 0;
+        return new UsersResource(User::take($take)->skip($skip)->get());
     }
 
     public function show(int $id): UserResource
